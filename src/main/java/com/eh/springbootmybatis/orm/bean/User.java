2a974e81486f13d4df47c83bf4ea88c297cc7951
david03.li <david03.li@vipshop.com>
2020-11-05 02:17:54 +0800

    feat(eg: 支付功能修改): eg: 支付接口

---
+++ src/main/java/com/eh/springbootmybatis/orm/bean/User.java
@@ -0,0 +1,13 @@
+package com.eh.springbootmybatis.orm.bean;
+
+@lombok.NoArgsConstructor
+@lombok.Data
+public class User {
+
+
+    private int id;
+    private String lastName;
+    private String email;
+    private int gender;
+    private int dId;
+}
--- src/test/java/com/eh/springbootmybatis/SpringbootMybatisApplicationTests.java
+++ src/test/java/com/eh/springbootmybatis/SpringbootMybatisApplicationTests.java
@@ -1,13 +1,21 @@
 package com.eh.springbootmybatis;
 
+import com.eh.springbootmybatis.orm.bean.Employee;
+import com.eh.springbootmybatis.orm.dao.EmployeeMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootMybatisApplicationTests {
 
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
     @Test
     void contextLoads() {
+        Employee employee = employeeMapper.selectByPrimaryKey(1);
+        System.out.println(employee);
     }
 
 }
